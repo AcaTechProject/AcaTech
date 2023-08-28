8ba99daf25040569ec137ff077d8fcce433197af
heeyoonsz <gmldbs0812@duksung.ac.kr>
2023-08-28 16:21:46 +0900

    Remove : example 폴더 삭제

--- src/main/java/com/example/heeyoon_hw_2/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.heeyoon_hw_2.Repository;
-
-import com.example.heeyoon_hw_2.Domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
