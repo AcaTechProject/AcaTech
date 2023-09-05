11887aafaa7075d6657b6a1951334be111b9c44f
KyungYoung Kim <87751531+KyungY-K@users.noreply.github.com>
2023-09-05 23:17:17 +0900

    feat: Add 경령 + 희윤 + 민지 진행 사항 전체

--- src/main/java/com/backend/AcaTech/Repository/Schedule/ScheduleRepository.java
+++ src/main/java/com/backend/AcaTech/Repository/Schedule/ScheduleRepository.java
@@ -2,17 +2,17 @@ package com.backend.AcaTech.Repository.Schedule;
 
 import com.backend.AcaTech.Domain.Schedule.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    List<Schedule> findByStartDateBetweenAndSchEdu(LocalDateTime startDate, LocalDateTime endDate, boolean schEdu);
-
-    List<Schedule> findByStartDateAndSchEdu(LocalDate startDate, boolean schEdu);
-
-    List<Schedule> findByStartDateAndSchEdu(LocalDateTime localDateTime, boolean schEdu);
-
     List<Schedule> findBySchIdIn(List<Long> schIds);
+
+    List<Schedule> findByStartDateBetweenAndSchEduOrSchCons(
+            LocalDateTime startDate, LocalDateTime endDate, boolean schEdu, boolean schCons);
+
+    List<Schedule> findByStartDateBetweenAndSchEdu(LocalDateTime localDateTime, LocalDateTime localDateTime1, boolean schEdu);
 }
