8477da3232409bf832aeb56d7fbc27293d827066
KyungYoung Kim <87751531+KyungY-K@users.noreply.github.com>
2023-09-05 23:28:05 +0900

    feat: Add 경령 + 희윤 + 민지 진행 사항 전체

--- src/main/java/com/backend/AcaTech/Domain/Schedule/Schedule.java
+++ src/main/java/com/backend/AcaTech/Domain/Schedule/Schedule.java
@@ -2,12 +2,8 @@ package com.backend.AcaTech.Domain.Schedule;
 
 import com.backend.AcaTech.Domain.Class.User;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 @Getter
@@ -25,31 +21,31 @@ public class Schedule {
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private User user;
 
-    @Column(name = "start_date", nullable = false)
+    @Column(name = "start_date")
     private LocalDateTime startDate;
 
-    @Column(name = "end_date", nullable = false)
+    @Column(name = "end_date")
     private LocalDateTime endDate;
 
-    @Column(name = "sch_title", nullable = false, length = 500)
+    @Column(name = "sch_title",length = 500)
     private String schTitle;
 
-    @Column(name = "sch_content", nullable = false, length = 1000)
+    @Column(name = "sch_content", nullable = true, length = 1000)
     private String schContent;
 
-    @Column(name = "sch_edu", nullable = false)
+    @Column(name = "sch_edu")
     private boolean schEdu;
 
-    @Column(name = "sch_cons", nullable = false)
+    @Column(name = "sch_cons")
     private boolean schCons;
 
     @Builder
     public Schedule(User user, LocalDateTime startDate, LocalDateTime endDate, String schTitle, String schContent, boolean schEdu, boolean schCons) {
-        this.user = user; // User 객체를 받아서 user 필드 초기화
+        this.user = user;
         this.startDate = startDate;
         this.endDate = endDate;
         this.schTitle = schTitle;
-        this.schContent = schContent;
+        this.schContent = schContent != null ? schContent : "";
         this.schEdu = schEdu;
         this.schCons = schCons;
     }
